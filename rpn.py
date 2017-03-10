4f2ebcd9c23581ff66ef1fbe983c2a238c2fc15b
Marcus M. Darden <mmdarden@umich.edu>
2017-03-10 14:54:05 -0500

    Finishing class...

--- rpn.py
+++ rpn.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 
-def add(arg1, arg2):
-	return arg1 + arg2
-
-def subtract(arg1, arg2):
-	return arg1 - arg2
+import operator
 
 OPERATORS = {
-	'+': add,
-	'-': subtract,
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul,
+	'/': operator.truediv,
 }
 
 
