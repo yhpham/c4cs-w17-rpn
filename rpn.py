2c35c4e3e091bba11234425edd7e30afe72f0c19
Marcus M. Darden <mmdarden@umich.edu>
2017-03-10 14:29:43 -0500

    Implement proper add for calculate

--- rpn.py
+++ rpn.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
-	pass
+	stack = list()
+	for operand in arg.split():
+		if operand == '+':
+			stack.append(stack.pop() + stack.pop())
+		else:
+			stack.append(float(operand))
+	return stack.pop()
 
 def main():
 	while True:
-		calculate(input('rpn calc> '))
+		result = calculate(input('rpn calc> '))
+		print("Result:", result)
 
 if __name__ == '__main__':
 	main()
